d04ae420ac63e7be410f851449196c3df93656bc
Vic van Gool <vic@cloud66.com>
2015-11-02 15:52:26 +0000

    adding adjustments for entering

--- app/models/image_generator.rb
+++ app/models/image_generator.rb
@@ -15,7 +15,10 @@ class ImageGenerator
 		txt.pointsize = (1.0 / 2.0) * size
 		txt.fill = 'white'
 		txt.font = 'Helvetica New'
-		image.annotate(txt, 0, 0, 0, 0, letters)
+
+		adjustment_x = -4.0/(200.0/size.to_f)
+		adjustment_y = 12.0/(200.0/size.to_f)
+		image.annotate(txt, 0, 0, adjustment_x, adjustment_y, letters)
 		return image
 	end
 end
