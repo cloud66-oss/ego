852b5758ec1030d6fe8b2caed1a3b81b7fe5b8b3
Vic van Gool <vic@cloud66.com>
2015-11-02 16:14:54 +0000

    basic cache

--- app/models/image_generator.rb
+++ app/models/image_generator.rb
@@ -4,21 +4,27 @@ class ImageGenerator
 	include Magick
 
 	def generate(letters, size: 100)
-		image = Image.new(size, size) {
-			self.background_color = '#cccccc'
-			self.gravity = Magick::CenterGravity
-			self.format = 'png'
-		}
+		path = "/cache/#{letters}_#{size}.png"
+		if File.exists?(path)
+			image = Image.read(path).first
+		else
+			image = Image.new(size, size) {
+				self.background_color = '#cccccc'
+				self.gravity = Magick::CenterGravity
+				self.format = 'png'
+			}
 
-		txt = Magick::Draw.new
-		txt.gravity = Magick::CenterGravity
-		txt.pointsize = (1.0 / 2.0) * size
-		txt.fill = 'white'
-		txt.font = 'Helvetica New'
+			txt = Magick::Draw.new
+			txt.gravity = Magick::CenterGravity
+			txt.pointsize = (1.0 / 2.0) * size
+			txt.fill = 'white'
+			txt.font = 'Helvetica New'
 
-		adjustment_x = -4.0/(200.0/size.to_f)
-		adjustment_y = 12.0/(200.0/size.to_f)
-		image.annotate(txt, 0, 0, adjustment_x, adjustment_y, letters)
+			adjustment_x = -4.0/(200.0/size.to_f)
+			adjustment_y = 12.0/(200.0/size.to_f)
+			image.annotate(txt, 0, 0, adjustment_x, adjustment_y, letters)
+			image.write(path)
+		end
 		return image
 	end
 end
