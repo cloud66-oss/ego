eda95d9231bf4bb13bb798df05f6884b39733257
Vic van Gool <lvangool@users.noreply.github.com>
2016-10-18 12:59:54 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 	get 'generate/:letters', :to => 'images#generate'
 	get 'generate/:letters/size/:size', :to => 'images#generate'
+	get '/', to: redirect('/404')
 end
